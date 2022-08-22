638858a304a89847a8fd0a0821770c9c72c48f59
Divyanshu Chaurasiya <72182690+Divyanshu960@users.noreply.github.com>
2022-08-23 01:24:27 +0530

    Create 125. Valid Palindrome.py

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s)).lower()
+        #print(s)
+        if (len(s)==1):
+            return "true"
+        return s == s[::-1
