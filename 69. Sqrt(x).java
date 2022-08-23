8a5423066bfc773d5dac623df4542bbeaa53b927
Divyanshu Chaurasiya <72182690+Divyanshu960@users.noreply.github.com>
2022-08-24 03:36:45 +0530

    Create 69. Sqrt(x).java

---
+++ 69. Sqrt(x).java
@@ -0,0 +1,10 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        {
+        int sqr = (int) Math.sqrt(x);
+        int result = (int) (sqr);
+        return result;
+    }
+    }
+}
