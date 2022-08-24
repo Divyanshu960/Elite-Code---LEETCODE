9080ee94d8836ea97296667e9538f7450b8a00fe
Divyanshu Chaurasiya <72182690+Divyanshu960@users.noreply.github.com>
2022-08-24 05:45:19 +0530

    Create 136. Single Number.java

---
+++ 136. Single Number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int size = nums.length;
+        int result = nums[0];
+        for (int i = 1; i < size; i++)
+            result = nums[i]^result;
+        return result;
+    }
+}
