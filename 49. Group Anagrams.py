22b938aa1a5e434dc23446543c0848843ab674b2
Divyanshu Chaurasiya <72182690+Divyanshu960@users.noreply.github.com>
2022-10-21 03:50:33 +0530

    Create 49. Group Anagrams.py

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strings: List[str]) -> List[List[str]]:
+        anagrams = dict()
+        anagramGroups = []
+        
+        for string in strings:
+            sortedString = "".join(sorted(string))
+            stringList = anagrams.get(sortedString)
+            
+            if stringList is None:
+                anagrams[sortedString] = []
+            anagrams[sortedString].append(string)
+        
+        
+        for sortedString in anagrams:
+            anagramGroups.append(anagrams[sortedString])
+        
+        return anagramGroups
